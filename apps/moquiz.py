55e2d3c583848640fbd09aad1d02be4c06cf0ca3
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-08-19 20:27:59 +0000

    added app

---
+++ apps/moquiz.py
@@ -0,0 +1,38 @@
+import time
+import os
+
+logo = '''
+ __    __     ______     ______     __  __     __     ______    
+/\ "-./  \   /\  __ \   /\  __ \   /\ \/\ \   /\ \   /\___  \   
+\ \ \-./\ \  \ \ \/\ \  \ \ \/\_\  \ \ \_\ \  \ \ \  \/_/  /__  
+ \ \_\ \ \_\  \ \_____\  \ \___\_\  \ \_____\  \ \_\   /\_____\ 
+  \/_/  \/_/   \/_____/   \/___/_/   \/_____/   \/_/   \/_____/ 
+'''
+
+print("Welcome to...\n")
+print(logo + "\n")
+print("Get ready for a series of challenges that will test your mind!")
+print("Sponsored by Yo Mom!")
+print("\n")
+
+# Main code
+choice1 = input("What inspired the creation of MochaOS?")
+if choice1 == "LeafOS":
+    choice2 = input("What year did the Berlin Wall fall?")
+    if choice2 == "1989":
+        choice3 = input("If Urainium is in something, what does it make it?")
+        if choice3 ==  "Radioactive":
+            print("That's all...")
+            quit()
+        else:
+            print("Wrong")
+            time.sleep(2)
+           quit()
+    else:
+        print("Wrong!")
+        time.sleep(2)
+        quit()
+else:
+    print("Wrong!")
+    time.sleep(2)
+    quit()

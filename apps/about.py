77c56fb52d2897dfca823eac9356642fcf55c523
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-08-19 22:05:49 +0000

    add version app

---
+++ apps/about.py
@@ -0,0 +1,9 @@
+# Made by Swarit Choudhari
+import time
+
+print("MochaOS ☕☕☕")
+print("v1.0.0\n")
+print("Made by Swarit Choudhari (A.K.A SwiftyProgrammer690")
+print("Inspired by leafboi111's LeafOS")
+time.sleep(5)
+quit()
--- os/os.sh
+++ os/os.sh
@@ -38,7 +38,7 @@ function main {
   while true
   do
     tput setaf 250
-    select input in mochaupd devrun 2048 mocalender nodecalc terminal mochapad One_Strike_And_You\'re_Out tetris moquiz
+    select input in about mochaupd devrun 2048 mocalender nodecalc terminal mochapad One_Strike_And_You\'re_Out tetris moquiz
     do
       cmpio "$input"
       clear
